68523d598711156ccc7cace9f63c72cfb64f75d1
Mauricio Muñoz <maumunoz@estudiantec.cr>
2021-01-18 19:25:45 -0600

    Catalogo Mariposas (falta el buscador también)

--- src/componentes/CatalogoMariposas.js
+++ src/componentes/CatalogoMariposas.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import Style from './../css/Style-Catalogo.css';
 import { Button } from "react-bootstrap";
-
+import axios from 'axios'
 class catalogoMariposas extends React.Component {
+
+
+    state = {
+        mariposas: []
+    }
+    componentDidMount(){
+        this.getMariposas()
+    }
+
+    getMariposas = async () => {
+        const mariposas = await axios.get("http://localhost:4000/api/species/getButterflies/")
+        console.log(mariposas.data.result)
+        this.setState({mariposas: mariposas.data.result})
+
+    }
+
+
     render() {
 
         return (
@@ -20,15 +37,24 @@ class catalogoMariposas extends React.Component {
                             <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Buscar</button>
                         <br/>
                     </form>
+                    <div className="row">
+                    {this.state.mariposas.map((mariposa => (
+                        <div className="col-md-4 auth_holder mr-10 p-2 " key={mariposa._id}>
 
-                        <a href="#">
-                            <div className="card" >
-                                <img className="catalogo" src="https://images.unsplash.com/photo-1566190777932-efa8ae5d2260?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1355&q=80" />
-                                <div className="card-body">
-                                    <h5 className="catalogo">Nombre</h5>
+                            <a href="#">
+
+                                
+                                <div className="card" >
+                                    <img className="catalogo" src={mariposa.photos[0]} />
+                                    <div className="card-body">
+                                        <h5 className="catalogo">{mariposa.name}</h5>
+                                    </div>
                                 </div>
-                            </div>
-                        </a>
+                            </a>
+                        </div>
+                        
+                        )))}
+                    </div>
                     </div>
             </div>
             
