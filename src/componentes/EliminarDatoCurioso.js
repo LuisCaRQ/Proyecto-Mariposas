e129f0482387ff818d64d492ec407181c9c7a80e
Mauricio Muñoz <maumunoz@estudiantec.cr>
2021-01-19 08:31:23 -0600

    delete dato curioso

--- src/componentes/EliminarDatoCurioso.js
+++ src/componentes/EliminarDatoCurioso.js
@@ -3,38 +3,59 @@ import Style from './../css/Style-Agregar_dato_curioso.css';
 import axios from 'axios'
 import {Button, Form } from "react-bootstrap";
 class EliminarDatoCurioso extends React.Component{
+    state= {
+        facts:[]
+    }
+    async componentDidMount(){
+        this.getFacts()
+    }
+
+    getFacts = async () =>{
+        const all =  await axios.get("http://localhost:4000/api/facts/getAll");
+        console.log(all.data.allFacts)
+        this.setState({facts:all.data.allFacts})
+    }
+
+    handleDelete = async ( id) => {
+        await axios.delete("http://localhost:4000/api/facts/delete/"+id)
+        this.getFacts()
+    }
     render(){
         
         return(
             <div>
                 <div id="fondoDato">
-                        <h1>Eliminar Datos curiosos</h1>
+                        <h1>Doble Click para eliminar dato curioso</h1>
                 </div>
 
                 <div Class="centerDatoCurioso">
                     <h4 class="Cuerpo"> Buscar dato curioso:</h4>
                     <form class="barra">
-                                <input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Search" />
-                                <br />
-                                <button class="btn btn-outline-secondary my-2 my-sm-0" type="submit">Buscar</button>
-                                <br />
+                               {this.state.facts.map((fact=>(
+
+                                <ul class="list-group" key={fact._id}>
+                                    <li class="list-group-item" >
+                                        {fact.fact} <button className="btn btn-danger m-4" onClick={() => this.handleDelete(fact._id)}>Borrar</button></li>
+
+                                    
+                                
+                                </ul>
+
+                               )))}
+                                
                             </form>
                     <br/> 
-                        <Form.Control as="textarea" rows="3" type="name" name="datoCurioso" disabled="true"/>
+                        
                     <br/> 
                 </div>
               
-                <div class="btn">
-                    <Button variant="danger" type="submit"  >
-                            Eliminar
-                    </Button>
-                </div>
+                
                 <div>
-                    <footer id="footerAbsolute">   
+                    {/* {<footer id="footerAbsolute">   
                         <div>Proyecto Metamorfosis</div>
                         <div>Versión 1.2 por Metamorfosis en Instituto Tecnológico de Costa Rica</div>
                         <div>Semestre II de 2020. Administración de Proyectos</div>            
-                    </footer>
+                    </footer>} */}
                 </div>
             </div>
         );
